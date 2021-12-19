da7bee94f444ec55b7a5e60f04e3d8f50e188b70
Kaden Butt <kadenjbutt@gmail.com>
2021-12-19 23:39:49 +0000

    First commit

---
+++ main.py
@@ -0,0 +1,20 @@
+from tkinter import *
+
+root = Tk()
+
+def Login_Checker(password, username):
+    pass
+
+if __name__ == '__main__':
+
+    # username_input = Entry(root, width=50, borderwidth=5)
+    # password_input = Entry(root, width=50, borderwidth=5)
+    # enter_button = Label(root, text="Press to enter")
+    #
+    # username_input.grid(row=0, column=0)
+    # password_input.grid(row=1, column=0)
+    # enter_button.grid(row=2, column=0)
+    # root.mainloop()
+    #
+
+    Login_Checker("Kaden", "Password")
