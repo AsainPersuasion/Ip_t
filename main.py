37969234a32400be6bbe43473702b36141d17764
Kaden <kaden@pop-os.localdomain>
2021-12-27 23:59:16 +0000

    added pinging to code not finished

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 import threading
 import tkinter
 from tkinter import *
-
-
+from pythonping import ping
+import datetime
 
 root = Tk()
 
-def Write_To_Txt(file_name, info):
-    pass
+start_ping_pressed = 0
 
 def Read_To_Txt(file_name):
     file_info = []
@@ -32,23 +31,70 @@ def Counter(label):
         i = i+1
         label.config(text=str(i))
 
+##function to write a string top .txt file
+def Write_To_Text(fileName, info):
+
+    with open(fileName + ".txt", "a") as f:
+        f.write("\n"+info)
+
+    f.close()
+
+##function which pings an ip
+def Target_Online(Ip):
+
+    resp = [ping(Ip, count=1, timeout = 1),ping(Ip, count=1, timeout = 1),ping(Ip, count=1, timeout = 1),ping(Ip, count=1, timeout = 1)]
+    offline = 0
+
+    for x in range(4):
+
+        if "Request" in str(resp[x]):
+            offline += 1
+
+    if offline < 4:
+        return True
+    else:
+        return False
 
 
-def main_screen():
+
+def Ping_Tracker(lable):
+    targetIp = "192.168.1.73"
+    amount_of_consetuive_disconnects = 0
+    amount_of_censetuive_connects = 0
+    while True:
+        if Target_Online(targetIp) == True and amount_of_consetuive_disconnects == 0 :
+            result = "Connected to device at " +str(datetime.datetime.now())
+            lable.config(text=result)
+            Write_To_Text("text", result)
+            amount_of_consetuive_disconnects+=1
+            amount_of_censetuive_connects = 0
+
+        if Target_Online(targetIp) == False and amount_of_censetuive_connects == 0:
+            result = "Disconnected to decive at " + str(datetime.datetime.now())
+            lable.config(text=result)
+            Write_To_Text("text", result)
+            amount_of_censetuive_connects+=1
+            amount_of_consetuive_disconnects = 0
+
+def Start_Pinging(lable):
+    counter_thread  = threading.Thread(target=Ping_Tracker, args = (lable,))
+    counter_thread.start()
+
+
+def Main_Screen():
 
     newWindow = tkinter.Toplevel(root,width=600, height=300)
     display_lable = Label(newWindow, borderwidth=5, text="test")
     ip_lable = Label(newWindow, text="Please enter your targets IP ")
     ip_input = Entry(newWindow)
-    start_attack_button = Button(newWindow, text="Press to start pinging your target")
+    start_attack_button = Button(newWindow, text="Press to start pinging your target", command=lambda: Start_Pinging(display_lable))
 
     display_lable.place(x=200, y=50)
     ip_lable.place(x=120,y=100)
     ip_input.place(x=310,y=100)
     start_attack_button.place(x=200,y=150)
+    # args = (ip_input, display_lable,)
 
-    counter_thread  = threading.Thread(target=Counter, args=(display_lable,))
-    counter_thread.start()
 
     root.mainloop()
 
@@ -71,7 +117,7 @@ def Login_Checker(username_needed, loggin_info):
 
     if access_to_system == True:
         print("Welcome to the system")
-        main_screen()
+        Main_Screen()
     else:
         print("access dineied")
 
@@ -88,8 +134,8 @@ if __name__ == '__main__':
     # password_input.grid(row=1, column=0)
     # enter_button.grid(row=2, column=0)
 
-    main_screen()
-    #
+    Main_Screen()
+
     # root.mainloop()
 
 
